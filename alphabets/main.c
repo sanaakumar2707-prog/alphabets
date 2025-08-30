76f7a6045d8e428bb04c9858ef995ffa5fedd58a
sanaa <Sanaakumar07@gmail.com>
2025-08-30 11:37:09 +0530

    c

--- alphabets/main.c
+++ alphabets/main.c
@@ -1,14 +1,8 @@
-//
-//  main.c
-//  alphabets
-//
-//  Created by Sanaa Kumar on 30/08/25.
-//
-
-#include <stdio.h>
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
-    return 0;
+#include<stdio.h>
+int main(void)
+{
+    char i='a';
+    for (i='a';i<='z';i++){
+        printf(" %c",i);
+    }
 }
